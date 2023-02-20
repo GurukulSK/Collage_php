5240d8939e5249bb8cd161ed2318980634d24015
gurukulsk <gurukulgetpass@gmail.com>
2023-02-20 08:12:06 +0530

    fina;

---
+++ 17_02.php
@@ -0,0 +1,11 @@
+<?php
+  // echo "Hello,\n World";
+  // Echo "1. me";
+  // Echo "\n2.Aanother thihng to do";
+  // echo "3.Learnm \"fun\"" ,"  ","towo","  ","therrere";
+  // echo "My namem is : "." Krish";
+
+  
+  $myname = "Kerish";
+  echo $myname;
+  ?>
---
+++ 20_02/variables.php
@@ -0,0 +1,9 @@
+<?php
+    $my_example = "My Value";
+    echo $my_example;
+    $biography = "\n This is a bio about god";
+    echo $biography;
+    $favorite_food = "\n"."I am"."true"."buick"."an";
+    echo $favorite_food;
+    $favorite_food = " "
+?>
